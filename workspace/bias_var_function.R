5f2d422b7f495663f0a815df5f695b59610cc060
matsgrvk <mats.garvik@gmail.com>
2020-07-24 17:42:25 +0200

    Changes results to detailed lists

--- workspace/bias_var_function.R
+++ workspace/bias_var_function.R
@@ -1,5 +1,4 @@
 bias_var <- function(var.future,gc.data){
-  nlabs = length(model_names)-1 ### Number of labels used in the GC. Needs to be total number -1 since C++ index starts from 0
   width = ncol(ref)
   height = nrow(ref)
   
@@ -9,18 +8,17 @@ bias_var <- function(var.future,gc.data){
     bias_future[,,i] <- var_future[,,i]- ref_future
   }
   
-  var_gc <- bias_gc <- array(0,c(nrow = height,ncol = width,length(model_names)))
+  var_gc <- bias_gc <- matrix(0,nrow = height,ncol = width)
   
   for(l in 0:(length(model_names)-2)){
-    islabel <- which(gc.data[,,m] == l)
-    var_gc[,,m][islabel] <- var_future[,,(l+1)][islabel]
-    bias_gc[,,m][islabel] <- bias_future[,,(l+1)][islabel]
+    islabel <- which(gc.data == l)
+    var_gc[islabel] <- var_future[,,(l+1)][islabel]
+    bias_gc[islabel] <- bias_future[,,(l+1)][islabel]
   }
   
-  bias_var_result <- vector("list",length=2)
+  bias_var <- vector("list",length=2)
   
-  bias_var_result <- list(var_gc,bias_gc)
+  bias_var <- list("Var" = var_gc,"Bias" = bias_gc)
   
-  return(bias_var_result)
+  return(bias_var)
 }
-
--- workspace/gc_function.R
+++ workspace/gc_function.R
@@ -1,11 +1,9 @@
-list.of.packages <- c("ncdf4","RcppXPtrUtils","paramtest","reshape2","devtools")
+list.of.packages <- c("RcppXPtrUtils","devtools")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages,repos = "http://cran.us.r-project.org")
 lapply(list.of.packages, library, character.only = TRUE)
-install_github("thaos/gcoWrapR")
+# install_github("thaos/gcoWrapR")
 library(gcoWrapR)
-
-t=0
 
 graphcut <- function(var.present,var.future,gc.type,weight.data,weight.smooth){
   
@@ -14,8 +12,8 @@ graphcut <- function(var.present,var.future,gc.type,weight.data,weight.smooth){
   ### weight.data and weight.smooth : number (1 for both is approximatly the optimal choice)
   
   nlabs = length(model_names)-1 ### Number of labels used in the GC. Needs to be total number -1 since C++ index starts from 0
-  width = ncol(ref)
-  height = nrow(ref)
+  width = ncol(ref) ### Number of longitudes
+  height = nrow(ref) ### Number of latitudes
   
   # Instanciation of the GraphCut environment
   
@@ -65,17 +63,17 @@ graphcut <- function(var.present,var.future,gc.type,weight.data,weight.smooth){
   bias_cpp = c(aperm(bias, c(2, 1, 3))) ### Permuting longitude and latitude since the indexing isn't the same in R and in C++
   bias_future_cpp = c(aperm(bias_future, c(2, 1, 3)))
   
-  if(gc.type == "present"){
+  if(gc.type == "present"){ ### Data and smooth cost calibrated on present data
     gco$setDataCost(ptrDataCost, list(numPix = width * height, data = bias_cpp, weight = weight.data))
     gco$setSmoothCost(ptrSmoothCost, list(numPix = width * height, data = bias_cpp, weight = weight.smooth))
   }
   
-  if(gc.type == "hybrid"){
+  if(gc.type == "hybrid"){ ### Data cost calibrated on present data and smooth cost calibrated on future data
     gco$setDataCost(ptrDataCost, list(numPix = width * height, data = bias_cpp, weight = weight.data))
     gco$setSmoothCost(ptrSmoothCost, list(numPix = width * height, data = bias_future_cpp, weight = weight.smooth))
   }
   
-  if(gc.type == "future"){
+  if(gc.type == "future"){ ### Data and smooth cost calibrated on future data
     gco$setDataCost(ptrDataCost, list(numPix = width * height, data = bias_future_cpp, weight = weight.data))
     gco$setSmoothCost(ptrSmoothCost, list(numPix = width * height, data = bias_future_cpp, weight = weight.smooth))
   }
@@ -86,12 +84,12 @@ graphcut <- function(var.present,var.future,gc.type,weight.data,weight.smooth){
     mae_list[[i]] <- mean(abs(bias[,,i]))
   }
   
-  mat_init <- bias[,,(which.min(mae_list))] ### Creating an initialization matrix
+  mat_init <- matrix(which.min(mae_list),nrow(ref),ncol(ref)) ### Creating the initialization matrix based on the best model from the previous list
   
   vec_init <- as.vector(t(mat_init)) ### Permuting the indexing from R to C++
   
-  for(z in 0:(length(mat_init)-1)){ 
-    gco$setLabel(z,vec_init[z+1]) ### Setting the labels for the alpha-beta swap algorithm
+  for(z in 0:(length(mat_init)-1)){ ### Setting the labels for the alpha-beta swap algorithm
+    gco$setLabel(z,vec_init[z+1]) 
   }
   
   # Optimizing the MRF energy with alpha-beta swap (-1 refers to the optimization until convergence)
@@ -100,33 +98,30 @@ graphcut <- function(var.present,var.future,gc.type,weight.data,weight.smooth){
   
   begin <- Sys.time()
   
-  gco$swap(-1)
+  gco$swap(-1) ### Optimization with alpha-beta swap algorithm
   
   time_spent <- Sys.time()-begin
   
   cat("GraphCut optimization done :  ")
   print(time_spent)
   
-  label_attribution <- array(0,c(nrow = height,ncol = width,length(model_names))) ### Each dimension contains the GraphCut result for a given reference
+  label_attribution <- matrix(0,nrow = height,ncol = width) ### Creating the label attribution matrix
   data_smooth_list <- vector("list",length(model_names)) ### Values of data and smooth energy for each GraphCut 
   
   data_cost <- gco$giveDataEnergy() ### Storing the energies for the GraphCut
   smooth_cost <- gco$giveSmoothEnergy()
   
-  t=t+1
-  
-  data_smooth_list[[t]] <- list("Data cost" = data_cost, "Smooth cost" = smooth_cost)
-  
-  tmp_label_attribution <- matrix(0, nrow=height, ncol=width) ### Creating the label attribution matrix
+  data_smooth_list <- list("Data cost" = data_cost, "Smooth cost" = smooth_cost)
   
   for(j in 1:height){
     for(i in 1:width){
-      tmp_label_attribution[j,i] <- gco$whatLabel((i - 1) + width * (j - 1)) ### Permuting from the C++ indexing to the R indexing
+      label_attribution[j,i] <- gco$whatLabel((i - 1) + width * (j - 1)) ### Permuting from the C++ indexing to the R indexing
     }
   }
   
-  label_attribution[,,t] <- tmp_label_attribution
+  gc_result <- vector("list",length=2)
   
-  return(label_attribution)
+  gc_result <- list("Label attribution" = label_attribution, "Data and smooth cost" = data_smooth_list)
+  
+  return(gc_result)
 }
-
