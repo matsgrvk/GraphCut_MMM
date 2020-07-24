59ab50a09b8c3d98f86130d797183649b1368d36
matsgrvk <mats.garvik@gmail.com>
2020-07-24 17:41:51 +0200

    Add data and smooth costs list to the gc function

--- label_attribution_bias_var_gc.R
+++ label_attribution_bias_var_gc.R
@@ -4,15 +4,15 @@ setwd("/homel/mgarvik/Bureau/Stage_M2/Codes/clean_files/workspace/")
 load("models_list.rdata")
 load(paste0("load_",var,"_cv.rdata"))
 
-##### Parameters for the GraphCut function ######
+##### GraphCut ######
 
 model_names <- TAS_MODELS[c(1,3,4,5,7,8,11,12,13,16,17,20,22,24,26,30,31,33,34,37),2] #### Names of models used in the ensemble
 
-# label_attribution <- array(0,length(model_names))
+gc_result <- list("vector",length(model_names)) ### List of label attribution matrices obtained with GraphCut
 
 source("gc_function.R")
 
-for(m in 1:length(model_names)){
+for(m in 1:2){
   
   ref <- get(paste0("tas_",model_names[[m]]))
   ref_future <- get(paste0("tas_",model_names[[m]],"_2100"))
@@ -27,7 +27,7 @@ for(m in 1:length(model_names)){
   var_present <- var_present[,,-m]
   var_future <- var_future[,,-m]
   
-  gc_result <- graphcut(var.present = var_present,
+  gc_result[[m]] <- graphcut(var.present = var_present,
                       var.future = var_future,
                       gc.type = "present",
                       weight.data = 1,
@@ -38,7 +38,9 @@ for(m in 1:length(model_names)){
 
 ##### Bias and var reconstruction for GraphCut result #####
 
-for(m in 1:length(model_names)){
+bias_var_result <- list("vector",length(gc_result)) ### List of variable and variable biases obtained with GraphCut
+
+for(m in 1:length(gc_result)){
   
   source("bias_var_function.R")
   
@@ -51,6 +53,6 @@ for(m in 1:length(model_names)){
   
   var_future <- var_future[,,-m]
   
-  bias_var_result <- bias_var(var.future = var_future,
-                              gc.data = gc_result)
+  bias_var_result[[m]] <- bias_var(var.future = var_future,
+                              gc.data = gc_result[[m]])
 }
